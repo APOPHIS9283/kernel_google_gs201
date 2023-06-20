9a3ca8a08794ff439d41580d2933d336966088b0
apophis9283 <steaven.android@gmail.com>
2023-06-19 20:51:33 -0400

    Small fix

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -983,9 +983,6 @@ static ssize_t comp_algorithm_show(struct device *dev,
 static ssize_t comp_algorithm_store(struct device *dev,
 		struct device_attribute *attr, const char *buf, size_t len)
 {
-	/* ignore userspace writes */
-	return len;
-
 	struct zram *zram = dev_to_zram(dev);
 	char compressor[ARRAY_SIZE(zram->compressor)];
 	size_t sz;
